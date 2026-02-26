0a7105c54a4f0b1f5ba15ac7a5cf8b8cf6d5da5a
Loc Lam <loc.lam@silentium.io>
2026-02-26 15:11:25 +0700

    feat(PrescriptionDelivery): add flow actions

---
+++ src/screens/PrescriptionDelivery/flow.ts
@@ -0,0 +1,8 @@
+import type { FlowAction } from '@/flow/types'
+
+export const actions: FlowAction[] = [
+  { trigger: 'ScreenHeader:Delivery Method', navigate: '/prescription-list' },
+  { trigger: 'RadioCard:Home Delivery', setState: 'home-delivery' },
+  { trigger: 'RadioCard:Apotheke Pickup', setState: 'apotheke-pickup' },
+  { trigger: 'Button:Continue', navigate: '/prescription-location' },
+]
